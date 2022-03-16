f758a94b69b8a6b75b9011b84ee144dae73310ea
Fabricio Velôso <fabriciovellozo@gmail.com>
2022-03-15 21:44:22 -0300

    Update project architecture

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ mylcd = lcddriver.lcd()
 while True:
   mylcd.lcd_display_string(time.strftime('%I:%M:%S %p'), 1)
   mylcd.lcd_display_string(time.strftime('%a %b %d, 20%y'), 2)
+  mylcd.lcd_display_string('Next alarm:',3)
+  mylcd.lcd_display_string('It so iz',4)
